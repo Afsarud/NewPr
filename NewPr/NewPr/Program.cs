bfc7e51d0e33b28e0c25bd8a60385fded1a329d7
Afsarud <engafsar5577@gmail.com>
2019-09-11 19:10:38 +0600

    Personal info

---
+++ NewPr/NewPr/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewPr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string fstName = "";
+            String secoundname = "";
+            string address = "";
+            string contact = "";
+
+            Console.WriteLine("Enter your First name Please : ");
+            fstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Second name Please : ");
+            secoundname = Console.ReadLine();
+
+            Console.WriteLine("Enter your Age Please : ");
+           age=Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter your address Please : ");
+            address = Console.ReadLine();
+
+            Console.WriteLine("Enter your contact Please :");
+            contact = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine(fstName + " " + secoundname + " " + age + " " + address + " " + contact);
+            
+            Console.ReadLine();
+        }
+    }
+}
